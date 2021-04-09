8c220ffbb9ff6bb7f009e326b2892942b24a2a25
DaivaM <merkiene.daiva@gmail.com>
2021-04-09 13:56:21 +0300

    Add calculator logic

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -10,6 +10,18 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter value 1:");
+            string var1 = Console.ReadLine();
+            Console.WriteLine("Enter value 2:");
+            string var2 = Console.ReadLine();
+
+            int number1 = int.Parse(var1);
+            int number2 = int.Parse(var2);
+
+            int result = number1 + number2;
+
+            Console.WriteLine($"Suma: { result}");
+            Console.ReadKey();
         }
     }
 }
